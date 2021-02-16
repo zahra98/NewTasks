ced28b36e4540be0a2f95140cad5efb190815431
zahra98 <zahraabuzahra4@gmail.com>
2021-02-17 00:50:18 +0200

    edit the book list

--- resources/views/allBooks.blade.php
+++ resources/views/allBooks.blade.php
@@ -17,11 +17,32 @@
                     <?php
                   //  echo count($books);
                     for ($x = 0; $x < count($books); $x++) {
+                        $bookNumber = $x +1;
+                        echo " $bookNumber - ";
+                        echo "<br> ";
+                        echo "Title : ";
                         echo "<h> ";
                         echo $books[$x]->title;
                         echo "</h>";
-                      echo "<br>";
-                      echo "<br>";
+                        echo "<br> ";
+                        echo "Auther : ";
+                        echo "<h> ";
+                        echo $books[$x]->auther;
+                        echo "</h>";
+                        echo "<br> ";
+                        echo "ISPN : ";
+                        echo "<h> ";
+                        echo $books[$x]->ispn;
+                        echo "</h>";
+                        echo "<br> ";
+                        echo "Available Copies : ";
+                        echo "<h> ";
+                        echo $books[$x]->copies;
+                        echo "</h>";
+
+
+                        echo "<br> ";
+                        echo "<br> ";
                       } 
                     ?>
                 </div>
