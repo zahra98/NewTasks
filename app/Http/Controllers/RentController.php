c8490b4b713271ef1282a95d44c61f9a9f74f0d0
zahra98 <zahraabuzahra4@gmail.com>
2021-02-22 00:05:44 +0200

    new tables..

--- app/Http/Controllers/RentController.php
+++ app/Http/Controllers/RentController.php
@@ -37,19 +37,6 @@ class RentController extends Controller
 
 
         Mail::to(  $toEmail )->send(new rentRequest($user,$books,$owner, $address));
-    //     Mail::raw('it works',function($message){
-    //    $current_user_email = Auth::User()->email;
-
-    //    $bookid = request('bookid');
-    //    $books = Book::where('id',  $bookid )->firstOrfail();
-    //    $owner_id = $books->user_id;
-    //    $owner = User::where('id',  $owner_id )->firstOrfail();
-      
-       
-    //         $owner_email = $owner->email;
-    //         $message->to($owner_email)->subject('hello there')->from($current_user_email);
-    //     });
-
        return view('/emailSent')->with('message','request sent') ;
 
 
---
+++ database/migrations/2021_02_21_214655_create_rented_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Carbon\Carbon;
+class CreateRentedTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+
+        Schema::create('rented', function (Blueprint $table) {
+            $start = Carbon::now();
+            $current = Carbon::now();
+            $dueDate = $current->addDays(3);
+            $table->id();
+            $table->string('status')-> default('pending');
+            $table->date('startDate')-> default($start);
+            $table->date('dueDate')-> default($dueDate);
+            $table->bigInteger('renter_id')->unsigned();
+            $table->bigInteger('rentedBook_id')->unsigned();
+            $table->bigInteger('request_id')->unsigned();
+         
+        });
+
+        Schema::table('rented', function( Blueprint $table) {
+   
+            $table->foreign('renter_id')
+            ->references('id')
+            ->on('users')
+            ->onDelete('cascade');
+
+            $table->foreign('rentedBook_id')
+            ->references('id')
+            ->on('books')
+            ->onDelete('cascade');
+
+            $table->foreign('request_id')
+            ->references('id')
+            ->on('requests')
+            ->onDelete('cascade');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rented');
+    }
+}
